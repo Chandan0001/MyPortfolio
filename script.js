61668c9b57d0cf94bb796eff27b922db2f93749a
Chandan <chandanraut94@gmail.com>
2023-08-12 04:20:01 +0530

    First release of my Portfolio

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed('#element', {
+    strings: ['Web Developer.', 'Experienced of Support Analyst', 'Stock Market Trader/Invester.'],
+    typeSpeed: 50,
+  });
+
+  // function downloadFile() {
+  //   window.open("./Resume")
+  // }
